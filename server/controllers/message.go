aa2ca08d499c04f0f52440f1e22d7bdbd1d5601c
Forever-Jimmy <m13203533462@163.com>
2021-10-04 11:46:45 +0800

    修改文件树

---
+++ server/controllers/message.go
@@ -0,0 +1,9 @@
+/*
+ * @Author: F1nley
+ * @Date: 2021-10-04 11:43:49
+ * @LastEditors: F1nley
+ * @LastEditTime: 2021-10-04 11:46:05
+ * @Description:
+ */
+
+package controllers
--- server/models/messages.go
+++ server/models/messages.go
@@ -2,7 +2,7 @@
  * @Author: F1nley
  * @Date: 2021-10-01 21:04:19
  * @LastEditors: F1nley
- * @LastEditTime: 2021-10-01 21:04:19
+ * @LastEditTime: 2021-10-04 11:43:42
  * @Description:
  */
 package models
@@ -11,5 +11,6 @@ type Message struct {
 	Id         int64  `xorm:pk autoincr`
 	Uid        int64  `xorm: notnull`
 	Content    string `xorm: notnull`
+	Anonymous  bool   `xorm: notnull`
 	CreateTime int64  `xorm: created notnull`
 }
